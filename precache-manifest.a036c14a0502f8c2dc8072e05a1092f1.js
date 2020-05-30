871d6aa0d7f960ca87f7142be2e54dc983d7776b
thedinindu <thedinindu@gmail.com>
2020-05-30 23:46:45 +0530

    Updates

---
+++ precache-manifest.a036c14a0502f8c2dc8072e05a1092f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17586518719e4b0c2404504db4f43724",
+    "url": "/RecipeApp-ReactJS/index.html"
+  },
+  {
+    "revision": "a470bbbc6f3c123c941c",
+    "url": "/RecipeApp-ReactJS/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2698f46b04a3799f2d56",
+    "url": "/RecipeApp-ReactJS/static/css/main.dc243b23.chunk.css"
+  },
+  {
+    "revision": "a470bbbc6f3c123c941c",
+    "url": "/RecipeApp-ReactJS/static/js/2.442ab2a7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/RecipeApp-ReactJS/static/js/2.442ab2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2698f46b04a3799f2d56",
+    "url": "/RecipeApp-ReactJS/static/js/main.b5bd26a5.chunk.js"
+  },
+  {
+    "revision": "49e4378fa29500e35f8f",
+    "url": "/RecipeApp-ReactJS/static/js/runtime-main.c9548ba1.js"
+  }
+]);
